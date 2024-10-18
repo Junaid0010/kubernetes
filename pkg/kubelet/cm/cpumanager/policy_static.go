e6337e83d0a19b7f770566c776cec7cd2644e35b
ajcaldelas <alan.caldelas@amd.com>
2024-10-18 19:26:27 +0000

    fixed more casing for UncoreCache

--- pkg/kubelet/cm/cpumanager/policy_static.go
+++ pkg/kubelet/cm/cpumanager/policy_static.go
@@ -503,7 +503,7 @@ func (p *staticPolicy) takeByTopology(availableCPUs cpuset.CPUSet, numCPUs int) 
 		return takeByTopologyNUMADistributed(p.topology, availableCPUs, numCPUs, cpuGroupSize, cpuSortingStrategy)
 	}
 
-	if p.options.PreferAlignByUnCoreCacheOption {
+	if p.options.PreferAlignByUncoreCacheOption {
 
 		return takeByTopologyUncoreCachePacked(p.topology, availableCPUs, numCPUs, cpuSortingStrategy)
 
